a57a03b1760560afbcbfaba33477dc1070430116
rbozek <rbozek@gmail.com>
2024-01-22 11:55:54 -0500

    functionality for add album. det route (POST to /albums). add UI. define route. import model to controller. code controller & export. test.

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,4 +1,4 @@
-// IMPORT MODEL UP HERE EVENTUALLY
+import { Album } from "../models/album.js"
 
 function index(req, res) {
   res.render('albums/index', {
@@ -12,6 +12,19 @@ function newAlbum(req, res) {
   })
 }
 
+function create(req, res) {
+  // console.log('create function first test');
+  Album.create(req.body)
+  .then(album => {
+    // console.log('create function 2nd test');
+    res.redirect('/albums')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/albums/error')
+  })
+}
+
 function myShelf(req, res) {
   res.render('albums/my-shelf', {
     title: 'My Shelf'
@@ -21,5 +34,6 @@ function myShelf(req, res) {
 export {
   index,
   newAlbum as new,
+  create,
   myShelf
 }
--- routes/albums.js
+++ routes/albums.js
@@ -5,16 +5,16 @@ const router = Router()
 
 // display main public shelf - GET http://localhost:3000/albums
 router.get('/', albumsCtrl.index)
-// replaces: 
-// router.get('/', function (req, res) {
-//   res.render('albums/index', { title: 'Shelver - public shelf' })
-// })
 
 // display new album page - GET http://localhost:3000/albums/new
 router.get('/new', albumsCtrl.new)
+// create new album - POST http://localhost:3000/albums/
+router.post('/', albumsCtrl.create)
 
 // display my collection page - GET http://localhost:3000/albums/collection
 router.get('/my-shelf', albumsCtrl.myShelf)
+
+
 
 
 export {
