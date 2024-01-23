02350d798343c64a8ea0fcb9ba3d705e51787a84
rbozek <rbozek@gmail.com>
2024-01-22 20:06:07 -0500

    begin reviews functionality. update Album model with embedded review schema.

--- models/album.js
+++ models/album.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  content: String,
+  rating: Number,
+  author: { type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
 const albumSchema = new Schema({
   title: {
     type: String,
@@ -13,7 +21,9 @@ const albumSchema = new Schema({
     required: true,
   },
   releaseYear: {type: Number},
-  recordLabel: {type: String}
+  recordLabel: {type: String},
+  reviews: [reviewSchema],
+  // owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
 },{ 
   timestamps: true
 })
